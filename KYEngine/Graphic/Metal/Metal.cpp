872b3ab17a96211b704bef7e426e3292d5c7f9c2
Kit <kit@kit-MBP.local>
2015-11-15 23:18:41 +0800

    parepre cmake files

---
+++ KYEngine/Graphic/Metal/Metal.cpp
@@ -0,0 +1,8 @@
+#include "stdafx.h"
+
+#include "Metal.h"
+
+namespace KY
+{
+	
+}
---
+++ KYEngine/Graphic/Metal/Metal.h
@@ -0,0 +1,11 @@
+#ifndef __METAL_H__
+#define __METAL_H__
+namespace Metal
+{
+	class MetalDevice
+	{
+	
+	}
+}
+
+#endif //__METAL_H__
